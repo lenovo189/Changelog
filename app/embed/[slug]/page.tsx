65003fdacb5b4b2e532ce0ef12ff718b39b571c3
rentednumber-design <rentednumber@gmail.com>
2026-01-09 20:06:14 +0500

    fix dynamic error

--- app/embed/[slug]/page.tsx
+++ app/embed/[slug]/page.tsx
@@ -1,3 +1,7 @@
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
+
 import { createAnonClient } from "@/lib/supabase/anon";
 import type { Metadata } from "next";
 import { notFound } from "next/navigation";
