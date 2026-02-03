d3e019eb3f095758373aea62da6450837ed7b149
rentednumber-design <rentednumber@gmail.com>
2026-02-03 19:38:24 +0500

    newfix

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -18,7 +18,7 @@ export default async function ProtectedPage() {
   } = await supabase.auth.getUser();
 
   if (!user) {
-    return redirect("/sign-in");
+    return redirect("/auth/login");
   }
 
   return (
